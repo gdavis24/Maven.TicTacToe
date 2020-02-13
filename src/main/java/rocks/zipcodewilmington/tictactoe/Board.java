69e6bd9bb538bf59084a0088f04b18e4aa249bd1
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-13 18:43:20 -0500

    finished tic tac toe

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,65 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
-    }
+    private Character[][] board;
 
+    public Board(Character[][] matrix) {
+        this.board = matrix;
+
+    }
     public Boolean isInFavorOfX() {
-        return null;
+        for (int i = 0; i < board.length; i++){
+            if (board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X'){
+                return true;
+            }
+            if (board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X'){
+                return true;
+            }
+            if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+                return true;
+            }
+            if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+                return true;
+            }
+
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        for (int i = 0; i < board.length; i++){
+            if (board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O'){
+                return true;
+            }
+            if (board[0][i] == 'O' && board[1][i] == 'O' && board[2][i] == 'O'){
+                return true;
+            }
+            if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+                return true;
+            }
+            if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+                return true;
+            }
+
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX() == false && isInFavorOfO() == false){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX() == true){
+            return "X";
+        }
+        if (isInFavorOfO() == true){
+            return "O";
+        }
+        return "";
     }
 
 }
